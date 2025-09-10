8b4d5f3dc7ff7d2aabc185ac482b5777a99273ed
Yang-goat <1700425119@qq.com>
2025-09-10 14:54:59 +0800

    项目初始化

---
+++ src/main/java/com/goatyang/cmbackend/CmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.goatyang.cmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goatyang/cmbackend/CmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goatyang.cmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
